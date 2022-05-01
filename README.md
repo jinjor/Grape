07a4aee4dc1adc6071d6eba753b21c50b4e37db9
Yosuke Torii <jinjorweb@gmail.com>
2022-05-01 22:17:56 +0900

    point the latest JUCE

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ code grape.code-workspace
 | ./debug-with-plugin-host.sh | Cmd+F5 |
 | ./debug-build.sh            | Cmd+F7 |
 | ./release-build.sh          |        |
+
+## Note
+
+- Needs a patch to debug: https://github.com/juce-framework/JUCE/pull/967
--- debug-build.sh
+++ debug-build.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+build_dir=build
+
 cmake \
-  -B build \
+  -B $build_dir \
   -G 'Unix Makefiles' \
   -D CMAKE_BUILD_TYPE=Debug \
   -D JUCE_COPY_PLUGIN_AFTER_BUILD=ON \
   -D JUCE_BUILD_EXTRAS=ON
   
-cmake --build build
+cmake --build $build_dir
